46e8f53ea75c682d419ba6e0672fd58cd4408a86
Ryan Mathisen <ryantmathisen@gmail.com>
2024-09-18 12:20:16 -0700

    add some simple js to bootstrap front-end behavior

--- WordleMVC/wwwroot/js/site.js
+++ WordleMVC/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿"use strict";
 
-// Write your JavaScript code.
+(() => {
+    let guess = "";
+    let keyboard_buttons = document.querySelectorAll(".keyboard-btn");
+
+    keyboard_buttons.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            let button_value = e.target.value;
+            if (button_value === "back") handleBack();
+            else if (button_value === "enter") handleEnter();
+            else handleCharacter(button_value);
+
+            populateGuess();
+        })
+    });
+
+    function handleBack() {
+        if (guess.length > 0) guess = guess.substring(0, guess.length - 1);
+    }
+
+    function handleEnter() {
+        if (guess.length == 5) {
+            console.log(`Submitting guess ${guess}`);
+            guess = "";
+        }
+    }
+
+    function handleCharacter(character) {
+        if (guess.length < 5) guess += character;
+    }
+
+    function populateGuess() {
+        let active_guess_slots = document.querySelectorAll(".active-guess-character");
+
+        for (var i = 0; i < 5; i++) {
+            active_guess_slots[i].innerHTML = guess[i] || "";
+        }
+    }
+})();
