45d1132fb5a2cffdd10c1fe9d5654ad0feeed091
Ryan Mathisen <ryantmathisen@gmail.com>
2024-09-20 21:16:22 -0700

    implement front-end guess resolution and victory animation

--- WordleMVC/wwwroot/js/site.js
+++ WordleMVC/wwwroot/js/site.js
@@ -2,9 +2,16 @@
 
 (() => {
     let correct_word_id = 0;
+    let game_over = false;
     let guess = "";
-    let guess_evaluation = [];
+    let guess_results = [];
     let keyboard_buttons = document.querySelectorAll(".keyboard-btn");
+    let num_guesses = 0;
+    let previous_guess = "";
+
+    const NO_MATCH = 0;
+    const PARTIAL_MATCH = 1;
+    const EXACT_MATCH = 2;
 
     keyboard_buttons.forEach((btn) => {
         btn.addEventListener("click", (e) => {
@@ -18,15 +25,16 @@
     });
 
     function handleBack() {
-        if (guess.length > 0) guess = guess.substring(0, guess.length - 1);
+        if (guess.length > 0 && !game_over) guess = guess.substring(0, guess.length - 1);
     }
 
     function handleEnter() {
-        if (guess.length == 5 && correct_word_id > 0) {
+        if (guess.length == 5 && correct_word_id > 0 && !game_over) {
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function () {
                 if (this.readyState === 4 && this.status == 200) {
-                    guess_evaluation = stringToArray(this.responseText);
+                    guess_results = stringToArray(this.responseText);
+                    resolveGuessResults();
                 }
             }
 
@@ -34,17 +42,60 @@
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.send(JSON.stringify({ 'correctWordID': correct_word_id, 'guess': guess }));
 
+            previous_guess = guess;
             guess = "";
         }
     }
 
     function handleCharacter(character) {
-        if (guess.length < 5) guess += character;
+        if (guess.length < 5 && !game_over) guess += character;
     }
 
     function stringToArray(str) {
         // str is in format "[0,0,0,0,0]"
         return str.substring(1, str.length - 1).split(',').map(x => parseInt(x));
+    }
+
+    function resolveGuessResults() {
+        let characters = document.querySelectorAll(".wordle-character");
+        let correct_guess = true;
+
+        for (var i = num_guesses * 5; i < (num_guesses * 5 + 5); i++) {
+            if (guess_results[i % 5] == EXACT_MATCH) {
+                characters[i].classList.add("exact-match");
+                getKey(previous_guess[i % 5]).classList.add("exact-match");
+            } else if (guess_results[i % 5] == PARTIAL_MATCH) {
+                characters[i].classList.add("partial-match");
+                getKey(previous_guess[i % 5]).classList.add("partial-match");
+                correct_guess = false;
+            } else {
+                characters[i].classList.add("no-match");
+                getKey(previous_guess[i % 5]).classList.add("no-match");
+                disableKey(previous_guess[i % 5]);
+                correct_guess = false;
+            }
+            characters[i].innerHTML = previous_guess[i % 5];
+        }
+
+        num_guesses++;
+        if (num_guesses >= 6 || correct_guess) game_over = true;
+        if (correct_guess) triggerVictory();
+    }
+
+    function getKey(c) {
+        return Array.from(document.querySelectorAll(".keyboard-btn")).filter((x) => x.value == c)[0];
+    }
+
+    function disableKey(c) {
+        getKey(c).disabled = true;
+    }
+
+    function triggerVictory() {
+        let characters = document.querySelectorAll(".wordle-character");
+
+        for (var i = (num_guesses - 1) * 5; i < (num_guesses - 1) * 5 + 5; i++) {
+            characters[i].classList.add("victory-anim");
+        }
     }
 
     function populateGuess() {
