86ddd6cf8709211a01d4c478bca58c492e3b1f5c
Ryan Mathisen <ryantmathisen@gmail.com>
2024-09-20 21:13:21 -0700

    change word comparison to uppercase both strings before comparison

--- WordleAPI/Models/WordModel.cs
+++ WordleAPI/Models/WordModel.cs
@@ -14,16 +14,19 @@
 
         public int[] CompareWith(string guess)
         {
+            guess = guess.ToUpper();
+            string chars = this.Chars.ToUpper();
+
             // Super duper idiomatic C# code..
             int[] Matches = new int[5];
 
-            for (int i = 0; i < this.Chars.Length; i++)
+            for (int i = 0; i < chars.Length; i++)
             {
-                if (guess[i] == this.Chars[i])
+                if (guess[i] == chars[i])
                 {
                     Matches[i] = (int)Match.ExactPosition;
                 }
-                else if (this.Chars.Contains(guess[i]))
+                else if (chars.Contains(guess[i]))
                 {
                     Matches[i] = (int)Match.SomewhereInWord;
                 }
